f960d0fe029612262c38f3215acca570419da569
davaponte <david.aponte@outlook.com>
2021-02-03 10:14:29 -0500

    Problema 10 resuelto. Problema 8 corregido.

--- p010.py
+++ p010.py
@@ -19,12 +19,13 @@
 #  MA 02110-1301, USA.
 #  
 
-# ~ Problema N.9:
-# ~ Dados tres puntos en el plano por sus coordenadas (x, y), realice un programa
-# ~ que indique si los mismos se encuentran sobre una misma recta (si son Puntos
-# ~ Colineales).
+# ~ Problema N.10:
+# ~ Diseñe un programa que indique si tres puntos dados por sus coordenadas (x, y)
+# ~ forman triángulo y en caso de formarlo debe indicarse que tipo de triángulo
+# ~ forma.
 
 from sys import exit
+from math import sqrt
 
 def InputValue(Msg):
     print(Msg, end='')
@@ -36,6 +37,9 @@ def InputValue(Msg):
         print('Error Capa 8. Debes ingresar un número, cacho cabrón!')
         exit(1)
 
+def Distancia(x1, y1, x2, y2):
+    return sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
 x, y = [], []
 for c in range(3):
     x.append(InputValue('Ingrese x' + str(c + 1) + ': '))
@@ -46,15 +50,28 @@ dxA = x[1] - x[0]
 dyB = y[2] - y[1] 
 dxB = x[2] - x[1] 
 
+IsTriangle = False
+
 if (dxA == 0) or (dxB == 0):
     if (dxA == 0) and (dxB == 0):
-        print('Los puntos son colineales.')
+        print('Los puntos no forman un triángulo')
     else:
-        print('Los puntos NO son colineales.')
+        print('Los puntos SÍ forman un triángulo')
+        IsTriangle = True
 else:
     if (dyA / dxA == dyB / dxB):
-        print('Los puntos son colineales.')
+        print('Los puntos no forman un triángulo')
     else:
-        print('Los puntos NO son colineales.')
+        print('Los puntos SÍ forman un triángulo')
+        IsTriangle = True
       
-
+if IsTriangle:
+    dA = Distancia(x[0], y[0], x[1], y[1])
+    dB = Distancia(x[1], y[1], x[2], y[2])
+    dC = Distancia(x[2], y[2], x[0], y[0])
+    if (dA == dB) and (dB == dC):
+        print('Triángulo equilátero')
+    elif (dA == dB) or (dB == dC):
+        print('Triángulo isóceles')
+    else:
+        print('Triángulo escaleno')
